dbfd2294d36227442e1c2fb7583605a0d1b4a064
danie_000 <dh081@hdm-stuttgart.de>
2017-11-01 21:51:34 +0100

    initial

--- system/Widgets/header.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: danie_000
- * Date: 30.10.2017
- * Time: 12:09
- */
---
+++ system/widgets/header.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danie_000
+ * Date: 30.10.2017
+ * Time: 12:09
+ */
+echo'
+<div class="divnav">
+
+    <ul>
+      <li class="logoschrift">Stone Shop</li>
+    </ul>
+<nav>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="#">Shop</a></li>
+      <li><a href="#">Collection</a></li>
+      <li><a href="index.php/ueber-uns.php">Über Uns</a></li>
+     </ul>
+</nav>
+</div>
+';
