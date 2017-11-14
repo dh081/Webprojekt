aae9762f3722664d579baa175f75c92999d8799e
danie_000 <dh081@hdm-stuttgart.de>
2017-11-14 12:22:03 +0100

    initial

---
+++ admin.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danie_000
+ * Date: 14.11.2017
+ * Time: 10:56
+ */
+echo ' <!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <title>Admin</title>
+  </head>
+  <body>
+  <div class="kopfleisteadmin">
+  <a href="https://mars.iuk.hdm-stuttgart.de/~dh081/index.php">Zum Shop</a>
+  </div>
+  <div class="menuadminaußen">
+    <div class="menuadmin">
+        <ul>
+            <li> <a href="#">Kategorien</a><li>
+            <li> <a href="#">Produkte</a></li>
+            <li> <a href="#">Bestellungen</a></li>
+        </ul>
+    </div>
+  <div class="bodyadmin">Test</div>
+</div>
+  </body>
+';
