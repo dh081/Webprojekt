f86cefb007f46bfcf2d28ef3e6108c59d1b3ad73
maltebitzenhofer <mb280@hdm-stuttgart.de>
2017-11-15 16:56:04 +0100

    footer bearbeitet

--- system/widgets/footer.php
+++ system/widgets/footer.php
@@ -36,7 +36,79 @@ echo '
 <br>
 <br>
 <br>
-<footer id="footer">
+<footer class="footer-total">
+    <div class="footer-left">
+        <h3>Stone Shop</h3>
+        <p class="footer-links">
+        <h4>Informationen</h4>
+            <ul>
+                <li><a href="#">Impressum</a></li>
+                <li><a href="#">Datenschutz</a></li>
+                <li><a href="#">AGB</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Widerrufsrecht & Widerrufsformular</a></li>
+                <li><a href="#">Versand- & Zahlungsbedingungen</a></li>
+            </ul>   
+        </p>
+    </div>    
+    <div class="footer-center">
+        <h4>Kontakt</h4>
+         <div> = $0
+            <i class="fa fa-map-marker">
+               ::before
+            </i>
+            <p> 
+                <span>Steinstraße 1, 12345 Steinhausen</span>
+                "Gallien"
+            </p>
+         </div>
+         <div>
+            <i class="fa fa-phone">
+            ::before
+            </i>
+            <p>+0 1234 56789</p>
+         </div>
+         <div>
+            <i class="fa fa-envelope">
+            ::before
+            </i>
+            <p>
+                <a href="mailto:support@stoneshop.de">support@stoneshop.de</a>
+            </p>
+         </div>
+    </div>
+    <div class="footer-right">
+        <h4>Follow us on</h4>
+        <div class="footer-sm-icons">
+            <a href="#">
+                <i class="fa fa-facebook">
+                ::before
+                </i>
+            </a>
+            <a href="#">
+                <i class="fa fa-twitter">
+                ::before
+                </i>
+            </a>
+            <a href="#">
+                <i class="fa fa-instagram">
+                ::before
+                </i>
+            </a>
+            <a href="#">
+                <i class="fa fa-youtube">
+                ::before
+                </i>
+            </a>       
+        </div>
+    </div>
+</footer>
+
+
+
+
+
+<!--
     <div class="container">
     <div class="row">
     <div class="col-sm-4">
@@ -70,14 +142,10 @@ Besuchen Sie auch unsere Kanäle. </br>
     </div>
     </div>
     </div>
-</footer>
+</footer> !-->
 <div class="footer-bottom">
-    <div class="container">
-        <div class="row">
-            <div class="copyright">
+   <div class="copyright">
 © by Obelix GmbH & Co. KG 2017 - In Stone we trust.
-            </div>
-        </div>
     </div>
 </div>
 </body>
