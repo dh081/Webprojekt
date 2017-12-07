d174619372d2eca3907c398698e67df3a7991c1d
danie_000 <dh081@hdm-stuttgart.de>
2017-12-07 11:21:58 +0100

    initial

---
+++ shop.php
@@ -0,0 +1,43 @@
+<?php
+echo ' <!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <title>Stone Shop</title>
+  </head>';
+include ("system/widgets/header.php");
+
+echo '
+<div>
+    <div class="bannershop">
+<div class="zumshop">
+Die besten Steine für Klein und Groß!
+</div>
+</div>
+<br>
+<h1 style="text-align: center;">Unser Sortiment an handverlesenen Qualitätssteinen</h1>
+<br>
+<div class="shopvorgeschlageneprodukte">
+    <div class="produkteshop">
+    Bild
+    <p>Hinkelstein</p>
+    </div>
+    <div class="produkteshop">
+    Bild
+    <p>Spitzer Stein</p>
+    </div>
+    <div class="produkteshop">
+    Bild
+    <p>Flacher Stein</p>
+    </div>
+    <div class="produkteshop">
+    Bild
+    <p>Flacher Stein</p>
+    </div>
+</div>
+<div class="shopkategorien">
+
+</div>
+</div>';
