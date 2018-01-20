5631d5b43ffa190012fcc00ad4e813fc8932f8c8
maltebitzenhofer <mb280@hdm-stuttgart.de>
2018-01-20 15:07:05 +0100

    initial

--- ueber-uns.php
+++ ueber-uns.php
@@ -18,13 +18,22 @@ echo ' <!DOCTYPE html>
   <body>';
 include ("system/widgets/header.php");
 echo '
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+<div class="aboutus">
+<div class="about">
+Herzlich Willkommen,<br>
+wir sind Asterix und Obelix aus Gallien. Für uns ist die Steinbearbeitung nicht nur ein Handwerk,
+es ist vielmehr eine Kunst. Wir pflegen und bearbeiten unsere Steine täglich und achten darauf,
+dass sie stets in einem optimalen Zustand für unseren Kunden sind. Der wohl wichtigste Stein für uns
+ist der Hinkelstein. Er ist majestätisch, groß, schwer und aus dem perfekten Material um alle möglichen
+Skulpturen herzustellen. Mit der Zeit und nach vielen Reisen um den gesamten Globus, lernten wir die Kunst
+der Steine mit ihren vielen Aspekten kennen. Der Stein war immer da und wird es auch immer sein. So ist es auch
+mit unserem Handwerk. Wir können mit Stolz behaupten, dass die Römer bis heute nicht das Geheimnis
+unseres Erfolges herausgefunden haben. Dadurch besitzen wir in ganz Gallien ein Monopol. 
+Wir freuen uns über Ihre <a href="kontakt.php">Anfrage</a> und hoffen, dass wir Ihre Wünsche entsprechend umsetzten können.
+</div>
+<div class="about_img">
+<img src="https://ds1.static.rtbf.be/article/image/624x351/7/7/2/114ef345097bdf3abe74fea739ceb278-1497529307.jpg">
+</div>
+</div>
 ';
 include ("system/widgets/footer.php");
